359aa255fd7c5ded83c50e40c0151dcdd86e5ed3
Murali Janardhanan <muralijan20@gmail.com>
2013-06-09 01:13:03 -0400

    Sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 SampleApp::Application.routes.draw do
-  
-  #get "static_pages/home"
-
   #get "users/new"
-  root to: 'static_pages#home'
-
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  #get "static_pages/home"
+  root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
